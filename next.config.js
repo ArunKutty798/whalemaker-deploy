6537f2b3c00db43b2211f0ee97b21b1ee58c5458
ArunKutty798 <arunkutty798@gmail.com>
2021-10-28 20:23:20 +0530

    files added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-}
+  externals: [
+    (function () {
+      var IGNORES = ["electron"];
+      return function (context, request, callback) {
+        if (IGNORES.indexOf(request) >= 0) {
+          return callback(null, "require('" + request + "')");
+        }
+        return callback();
+      };
+    })(),
+  ],
+};
